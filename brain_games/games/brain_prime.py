e37fb869d2cc55149bff92c163a50ee3de897461
ggByron <shtyrbu.ya@gmail.com>
2023-01-07 13:36:02 +0500

    fixes for number two in  brain-prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,15 +5,17 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def make_question():
-    number = randint(1, 99)
+    number = randint(1, 5)
     question = f'Question: {number}'
     return question
 
 
 def correct_answer(question):
     number = int(question[9:])
-    divisor = 2
     correct_answer = 'yes'
+    if number == 1 or 2 or 3:
+        return correct_answer
+    divisor = 2
     while divisor <= (number / 2 + 1):
         if number % divisor == 0:
             correct_answer = 'no'
