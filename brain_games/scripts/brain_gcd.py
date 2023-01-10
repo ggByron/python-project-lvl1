08582c1b3519f8fe135e619e21f0d0e2c2340a21
ggByron <shtyrbu.ya@gmail.com>
2023-01-10 12:59:08 +0500

    update import in brain_gcd to script to reflect naming change from brain_gcd to gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games import engine
-from brain_games.games import brain_gcd
+from brain_games.games import gcd
 
 
 def main():
-    engine.play(brain_gcd)
+    engine.play(gcd)
 
 
 if __name__ == '__main__':
